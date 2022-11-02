b32d43a18b69a5b69d75720a2265bb11de4bcf6f
AndyRCol <lucho_chapu@hotmail.com>
2022-11-02 14:00:40 -0500

    Andy

---
+++ schema/usersSchema.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+const id = Joi.string().uuid();
+const name = Joi.string().min(3).max(15);
+const price = Joi.number().integer().min(10);
+
+const createUserSchema = Joi.object({
+    name: name.required(),
+    price: price.required(),
+
+});
+
+const updateUserSchema = Joi.object({
+    name: name,
+    price: price
+
+});
+
+const getUserSchema = Joi.object({
+    id: id.required(),
+});
+
+module.exports = { createUserSchema , updateUserSchema , getUserSchema }
---
+++ services/usersService.js
@@ -0,0 +1,76 @@
+
+const faker = require('faker');
+const boom = require('@hapi/boom');
+
+class usersService {
+
+constructor(){
+    this.users = [];
+    this.generate();
+
+}
+generate(){
+    const limit = 100;
+    for(let index = 0; index < limit; index++){
+        this.users.push({
+            id: faker.datatype.uuid(),
+            name: faker.commerce.productName(),
+            price: parseInt(faker.commerce.price(),10),
+            Image: faker.image.imageUrl(),
+            isBlock: faker.datatype.boolean(),
+
+        })
+    }
+
+}
+async Create(data){
+    const newUser = {
+        id: faker.datatype.uuid(),
+        ...data
+    }
+    this.users.push(newUser);
+    return newUser;
+}
+async find(){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            resolve(this.users)
+        }, 2000);
+    })
+    return this.users;
+}
+async findOne(id){
+const  user = this.users.find(item => item.id === id);
+if (!user){
+    throw boom.notFound('User not found1');
+}
+if(user.isBlock){
+    throw boom.conflict('User isBlock');
+}
+return user;
+}
+async update(id, changes){
+    const index = this.users.findIndex(item => item.id === id);
+    if (index === -1){
+        throw boom.notFound('User not found2');
+    }
+    const users = this.users[index];
+    this.users[index] = {
+        ...users,
+        ...changes
+    };
+    return this.users[index];
+}
+async delete(id){
+    const index = this.users.findIndex(item => item.id === id);
+    if (index === -1){
+        throw boom.notFound('User not found');
+    }
+    this.users.splice(index, 1);
+    return{message: true
+    ,id}
+}
+}
+
+
+module.exports = usersService;
